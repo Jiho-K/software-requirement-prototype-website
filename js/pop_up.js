92be5647f5b0ed9db749f1d331ff0213ab9d72cd
Jiho <jiho.kim@ucalgary.ca>
2021-04-10 13:45:26 -0600

    clean up code

--- js/pop_up.js
+++ js/pop_up.js
@@ -1,9 +1,9 @@
+// shows popup email for saving car
 function show_popup() {
   document.getElementById("email_popup").style.display = "block";
-  // document.getElementById("email_popup").style.visibility = "visible";
 }
 
+// hides popup email for saving car
 function close_popup() {
-  // document.getElementById("email_popup").style.visibility = "hidden";
   document.getElementById("email_popup").style.display = "none";
 }
