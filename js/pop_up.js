1e8c0b55ec716b8491a370d1271ba0b43490701a
EddieSKim <59934288+EddieSKim@users.noreply.github.com>
2021-04-10 13:41:35 -0600

    Added comments and removed commented out code

--- js/pop_up.js
+++ js/pop_up.js
@@ -1,9 +1,9 @@
+// Displaying email pop up when selected
 function show_popup() {
   document.getElementById("email_popup").style.display = "block";
-  // document.getElementById("email_popup").style.visibility = "visible";
 }
 
+// Remove email pop up when cancelled
 function close_popup() {
-  // document.getElementById("email_popup").style.visibility = "hidden";
   document.getElementById("email_popup").style.display = "none";
 }
